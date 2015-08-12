6d0d8ee6ca079887324e2b2b2cee4388a8648b76
Johan Oakes <oakejp12@wfu.edu>
2015-08-12 09:28:58 -0700

    Complete MongoDB configuration

--- routes/form.js
+++ routes/form.js
@@ -2,10 +2,10 @@
 'use strict';
 
 var express = require('express'),
-    router = express.Router(),
-    MongoClient = require('mongodb').MongoClient,
-    assert = require('assert'),
-    Qs = require('qs');
+  router = express.Router(),
+  MongoClient = require('mongodb').MongoClient,
+  assert = require('assert'),
+  Qs = require('qs');
 
 // TODO : Production - need to change this url
 var url = 'mongodb://localhost:27017/pdf_viewer';
@@ -15,37 +15,35 @@ var url = 'mongodb://localhost:27017/pdf_viewer';
 
 
 /* POST form */
-router.post('/', function(req,res) {
+router.post('/', function (req, res) {
 
-    console.log('*** Form Data below! ***');
-    console.dir(req.body);
+  console.log('*** Form Data below! ***');
+  console.dir(req.body);
 
+  MongoClient.connect(url, function (err, db) {
+    assert.equal(null, err);
 
-    MongoClient.connect(url, function (err, db) {
-        assert.equal(null,err);
+    var collection = db.collection('pdf_models');
+    var doc = req.body;
 
-        var collection = db.collection('pdf_models');
-        var doc = req.body;
-
-        // collection.update(selector, document, {upsert: true})
-        // { Name: "Johan Oakes", Result : { }}
-        collection.update({"Name": doc.Name}, { $addToSet: { "Result" : { "PDF" : doc.PDF, "Score" : doc.Score }}}, { upsert: true});
-
-        /*
-        // If "Name" already exists (person has already submitted a document) then update
-        if ((collection.find({"Name" : doc.Name.toString()}).count()) > 0) {
-            console.log('Document exists already so updating that one!');
-        } else { // Insert a new document
-            collection.insertOne(doc, function (err, result) {
-                assert.equal(err, null);
-                console.log('Inserted a document: ' + JSON.stringify(doc));
-                db.close();
-            });
+    // collection.update(selector, document, {upsert: true})
+    // { Name: "Johan Oakes", Result : { }}
+    collection.update({
+      "Name": doc.Name
+    }, {
+      $addToSet: {
+        "Result": {
+          "PDF": doc.PDF,
+          "Score": doc.Score,
+          "Print_S": doc.S_Print
         }
-        */
+      }
+    }, {
+      upsert: true
     });
+  });
 
-    res.send('View the next pdf selection.');
+  res.send('View the next pdf selection.');
 });
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,125 +1,129 @@
 /*jslint node: true */
 'use strict';
 
+
+/**
+ * TODO: Refactor the GET method from readFiles.js
+ *
+ */
+
+
 var express = require('express'),
-    router = express.Router();
+  router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
 
-    /**
-     * Created by Johan Oakes on 7/28/15.
-     * TODO : Refactor this from '/public/javascripts/readFiles.js'
-     */
+  var express = require('express'),
+    util = require('util'),
+    assert = require('assert'),
+    fs = require('fs');
 
-
-    var express = require('express'),
-        util = require('util'),
-        assert = require('assert'),
-        fs = require('fs');
-
-    var mongo = require('mongodb'),
-        db = new mongo.Db('pdfs', new mongo.Server('127.0.0.1', 27017)),
-        MongoClient = mongo.MongoClient,
-        Grid = require('gridfs-stream'),
-        gfs;
+  var mongo = require('mongodb'),
+    db = new mongo.Db('pdfs', new mongo.Server('127.0.0.1', 27017)),
+    MongoClient = mongo.MongoClient,
+    Grid = require('gridfs-stream'),
+    gfs;
 
 
-    db.open(function(err, db) {
-        if (err) throw err;
-        gfs = new Grid(db, mongo);
+  db.open(function (err, db) {
+    if (err) throw err;
+    gfs = new Grid(db, mongo);
+  });
+
+  // TODO : Change URL for production system
+  var url = 'mongodb://localhost:27017/pdfs';
+
+  function connectToMongo(callback) {
+    MongoClient.connect(url, callback);
+  }
+
+  /**
+   * Get filenames for fs.files
+   * TODO : Get filenames based on version
+   * TODO : Abstract MongoClient.connect function
+   * @returns []
+   */
+  function getFiles(callback) {
+
+    connectToMongo(function (err, db) {
+      assert.equal(null, err);
+
+      console.log('*** Reading filenames : MongoClient.connect() ***');
+
+      readDocuments(db, function (data) {
+        callback(data);
+        db.close();
+      });
     });
+  }
 
-    var url = 'mongodb://localhost:27017/pdfs';
+  /**
+   * Read documents from MongoDB
+   * Procedural :: readDocuments -> nameSeparation -> randomizedArray
+   * @param db ::
+   */
+  var readDocuments = function (db, callback) {
+    var nameSeparated = [],
+      U_names = [],
+      S_names = [],
+      randomizedArray = [];
+    var wantedPdfs = 20; // TODO : manipulate via wanted pdf test cluster
+    // Collection to read from
+    var collection = db.collection('fs.files');
 
-    function connectToMongo(callback) {
-        MongoClient.connect(url, callback);
-    }
+    collection.find({}).toArray(function (err, docs) {
+      assert.equal(err, null);
 
-    /**
-     * Get filenames for fs.files
-     * TODO : Get filenames based on version
-     * TODO : Abstract MongoClient.connect function
-     * @returns []
-     */
-    function getFiles(callback) {
+      if (docs === null) {
+        console.log("*** Closing database : couldn't read filenames ***");
+        db.close();
+      }
 
-        // TODO : Change URL for production system
-        connectToMongo(function (err, db) {
-            assert.equal(null, err);
+      console.log('Found %d documents when reading from database', docs.length);
 
-            console.log('*** Reading filenames : MongoClient.connect() ***');
+      nameSeparated = nameSeparation(docs);
+      U_names = nameSeparated[0].slice(0);
+      S_names = nameSeparated[1].slice(0);
 
-            readDocuments (db, function(data) {
-                callback(data);
-                db.close();
-            });
-        });
-    }
+      randomizedArray = randomizeArray(U_names, S_names, wantedPdfs);
 
-    /**
-     * Read documents from MongoDB
-     * Procedural :: readDocuments -> nameSeparation -> randomizedArray
-     * @param db ::
-     */
-    var readDocuments = function(db, callback) {
-        var nameSeparated = [], U_names = [], S_names = [], randomizedArray = [];
-        var wantedPdfs = 20; // TODO : manipulate via wanted pdf test cluster
+      U_names = randomizedArray[0].slice(0);
+      S_names = randomizedArray[1].slice(0);
 
-        // Collection to read from
-        var collection = db.collection('fs.files');
-
-        collection.find({}).toArray(function (err, docs) {
-            assert.equal(err, null);
-
-            if (docs === null) {
-                console.log("*** Closing database : couldn't read filenames ***");
-                db.close();
-            }
-
-            console.log('Found %d documents when reading from database', docs.length);
-
-            nameSeparated   = nameSeparation(docs);
-            U_names         = nameSeparated[0].slice(0);
-            S_names         = nameSeparated[1].slice(0);
-
-            randomizedArray = randomizeArray(U_names, S_names, wantedPdfs);
-
-            U_names         = randomizedArray[0].slice(0);
-            S_names         = randomizedArray[1].slice(0);
-
-            callback([U_names, S_names]);
-        });
-    };
+      callback([U_names, S_names]);
+    });
+  };
 
 
 
-    /**
-     *
-     * Isolate filenames from docs returned by MongoDB into two arrays :
-     *  One array is filled with all simplified pdfs, the other is unsimplified pdfs
-     * @param array stores 'filename' from JSON array
-     * @returns {*[]} a multidimensional array holding U and S files
-     */
-    function nameSeparation(array) {
-        if (array.length > 0) {
-            var S_names = [], U_names = [];
+  /**
+   *
+   * Isolate filenames from docs returned by MongoDB into two arrays :
+   *  One array is filled with all simplified pdfs, the other is unsimplified pdfs
+   * @param array stores 'filename' from JSON array
+   * @returns {*[]} a multidimensional array holding U and S files
+   */
+  function nameSeparation(array) {
+    if (array.length > 0) {
+      var S_names = [],
+        U_names = [];
 
-            for (var i = 0, j = array.length; i < j; i++) {
-                var key = array[i]['filename'];
-                if (key.startsWith("S_"))
-                    S_names.push(key);
-                else if (key.startsWith("U_"))
-                    U_names.push(key);
-                else console.log("Couldn't find an array for: " + key);
-            }
+      for (var i = 0, j = array.length; i < j; i++) {
+        var key = array[i]['filename'];
+        if (key.startsWith("S_"))
+          S_names.push(key);
+        else if (key.startsWith("U_"))
+          U_names.push(key);
+        else console.log("Couldn't find an array for: " + key);
+      }
 
-            // Sort both arrays alphabetically
-            S_names.sort();
-            U_names.sort();
+      // Sort both arrays alphabetically
+      S_names.sort();
+      U_names.sort();
 
-            // Check whether they match
-            /*
+      // Check whether they match
+      /*
              for (i = 0, j = U_names.length; i < j; i++) {
              if (U_names.length != S_names.length) throw "Arrays aren't the same size!";
              if (U_names[i].substr(2, U_names[i].length) === S_names[i].substr(2, S_names[i].length))
@@ -127,69 +131,72 @@ router.get('/', function(req, res) {
              }
              */
 
-            return [U_names, S_names];
-        }
-        else {
-            console.log("Array of filenames is empty!");
-        }
+      return [U_names, S_names];
+    } else {
+      console.log("Array of filenames is empty!");
+    }
+  }
+
+  /**
+   * Utility function to separate S_ and U_ pdfs
+   * @param needle: what we want to find in the string
+   * @returns {boolean}: whether needle is in string or not
+   */
+  String.prototype.startsWith = function (needle) {
+    return (this.indexOf(needle) === 0);
+  };
+
+  /**
+   * Sort arrays s.t. both arrays have same pdf relative name
+   * @param u_array
+   * @param s_array
+   * @param wnumber
+   * @returns {*[]} a multidimensional array holding random U and S files
+   */
+  function randomizeArray(u_array, s_array, wnumber) {
+    console.log('*** Number of PDFs wanted: %d ***', wnumber);
+    // Holds a random range between 0:10
+    var range = [],
+      newU = [],
+      newS = [];
+
+    // Math.floor(Math.random() * (max - min + 1)) + 1
+    var randomNumber = Math.floor(Math.random() * (34 - 1 + 1)) + 1;
+
+    // Generate random array
+    // TODO : The modular number changes based on how many pdfs are available
+    for (var i = 0; i < wnumber; i++) {
+      // Do it mod 35 since we don't want (34 mod 34) === 0 => pfd at index 34 will never be shown
+      range.push((randomNumber + i) % 35);
     }
 
-    /**
-     * Utility function to separate S_ and U_ pdfs
-     * @param needle: what we want to find in the string
-     * @returns {boolean}: whether needle is in string or not
-     */
-    String.prototype.startsWith = function(needle) {
-        return(this.indexOf(needle) === 0);
-    };
-
-    /**
-     * Sort arrays s.t. both arrays have same pdf relative name
-     * @param u_array
-     * @param s_array
-     * @param wnumber
-     * @returns {*[]} a multidimensional array holding random U and S files
-     */
-    function randomizeArray(u_array, s_array, wnumber) {
-        console.log('*** Number of PDFs wanted: %d ***', wnumber);
-        // Holds a random range between 0:10
-        var range = [], newU = [], newS = [];
-
-        // Math.floor(Math.random() * (max - min + 1)) + 1
-        var randomNumber = Math.floor(Math.random() * (34 - 1 + 1)) + 1;
-
-        // Generate random array
-        // TODO : The modular number changes based on how many pdfs are available
-        for (var i = 0; i < wnumber; i++) {
-            // Do it mod 35 since we don't want (34 mod 34) === 0 => pfd at index 34 will never be shown
-            range.push((randomNumber + i) % 35);
-        }
-
-        var j;
-        for (i = 0, j = range.length; i < j; i++) {
-            var index = range[i];
-            newU.push(u_array[index]); // Holds the unsimplified pdfs to test
-            newS.push(s_array[index]); // Holds the simplified pds to test
-        }
-
-        return [newU, newS];
+    var j;
+    for (i = 0, j = range.length; i < j; i++) {
+      var index = range[i];
+      newU.push(u_array[index]); // Holds the unsimplified pdfs to test
+      newS.push(s_array[index]); // Holds the simplified pds to test
     }
 
-    /**
-     * TODO : I have the random file names, now I need to render them to index.jade
-     *  array in getFiles already has the filenames we want
-     *  We just need to fetch the actual PDF data from MongoDB to render them
-     */
-    getFiles(function(array){
-        res.render('index', {data: JSON.stringify(array)});
+    return [newU, newS];
+  }
+
+  /**
+   * TODO : I have the random file names, now I need to render them to index.jade
+   *  array in getFiles already has the filenames we want
+   *  We just need to fetch the actual PDF data from MongoDB to render them
+   */
+  getFiles(function (array) {
+    res.render('index', {
+      data: JSON.stringify(array)
     });
+  });
 
 
-    // res.render('index');
+  // res.render('index');
 });
 
 
-router.post('/', function(req, res) {
+router.post('/', function (req, res) {
 
 });
 
